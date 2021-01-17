481eb1dab65d620e8f7d2c019b4520ab96ec24fd
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-17 13:21:38 +0300

    book input functionality

--- app/src/main/java/com/moringaschool/mylib/MainActivity.java
+++ app/src/main/java/com/moringaschool/mylib/MainActivity.java
@@ -3,12 +3,23 @@ package com.moringaschool.mylib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etBookTitle;
+    private TextView tvAuthor, tvTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etBookTitle = findViewById(R.id.bookInput);
+    }
+
+    public void searchBooks(View view) {
+
     }
 }
