624f28d57dff346bcc3c70f49e591472dc8dae9a
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-17 16:03:00 +0300

    override annotation

---
+++ app/src/main/java/com/moringaschool/mylib/FetchBook.java
@@ -0,0 +1,11 @@
+package com.moringaschool.mylib;
+
+import android.os.AsyncTask;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
