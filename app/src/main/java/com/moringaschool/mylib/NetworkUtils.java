355cdf145fd5b251a52722dade2c71b2ec83f3f0
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-17 17:16:10 +0300

    try and catch error

--- app/src/main/java/com/moringaschool/mylib/NetworkUtils.java
+++ app/src/main/java/com/moringaschool/mylib/NetworkUtils.java
@@ -1,5 +1,29 @@
 package com.moringaschool.mylib;
 
+import android.app.DownloadManager;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+
 public class NetworkUtils {
     private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    //Base URI for the Books API
+    private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
+
+    //Parameter for the search string
+    private static final String QUERY_PARAM = "q";
+
+    //Parameter that limits search results
+    private static final String MAX_RESULTS = "maxResults";
+
+    //Parameter to filter by print type
+    private static final String PRINT_TYPE = "printType";
+
+    static String getBookInfo(String queryString){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+        return bookJSONString;
+    }
 }
